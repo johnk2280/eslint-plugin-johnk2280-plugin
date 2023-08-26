7120f03252aecf72a38b3086279e6e94d2581ae7
johnk2280 <johnk2280@gmail.com>
2023-08-26 15:10:15 +0500

    [upd] .eslintrc.js - added rules, path-checker.js

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,19 +1,32 @@
-"use strict";
+'use strict';
 
 module.exports = {
-  root: true,
-  extends: [
-    "eslint:recommended",
-    "plugin:eslint-plugin/recommended",
-    "plugin:node/recommended",
-  ],
-  env: {
-    node: true,
-  },
-  overrides: [
-    {
-      files: ["tests/**/*.js"],
-      env: { mocha: true },
+    root: true,
+    extends: [
+        'eslint:recommended',
+        'plugin:eslint-plugin/recommended',
+        'plugin:node/recommended',
+    ],
+    env: {
+        node: true,
     },
-  ],
+    overrides: [
+        {
+            files: ['tests/**/*.js'],
+            env: { mocha: true },
+        },
+    ],
+    rules: {
+        'quotes': ['error', 'single'],
+        'indent': [2, 4],
+        'react/jsx-indent': [2, 4],
+        'react/jsx-indent-props': [2, 4],
+        'no-unused-vars': 'warn',
+        'eslint-plugin/require-meta-type': 'warn',
+        'prefer-message-ids': 'warn',
+        'semi': 'off',
+        '@typescript-eslint/semi': ['error', 'always'],
+        'object-curly-spacing': ['warn', 'always'],
+        'react/jsx-curly-spacing': ['warn', { 'when': 'always', children: true }]
+    }
 };
