e7fe905fb3205d9d2231a1ac57bba61b8fc2e606
johnk2280 <johnk2280@gmail.com>
2023-10-15 17:17:33 +0500

    [add] tests for path-checker

--- tests/lib/rules/path-checker.js
+++ tests/lib/rules/path-checker.js
@@ -1,31 +1,27 @@
-/**
- * @fileoverview FSD relative path checker
- * @author johnk2280
- */
-"use strict";
-
-//------------------------------------------------------------------------------
-// Requirements
-//------------------------------------------------------------------------------
-
-const rule = require("../../../lib/rules/path-checker"),
-  RuleTester = require("eslint").RuleTester;
+const rule = require('../../../lib/rules/path-checker'),
+    RuleTester = require('eslint').RuleTester;
 
 
-//------------------------------------------------------------------------------
-// Tests
-//------------------------------------------------------------------------------
+const ruleTester = new RuleTester({
+    parserOptions: {
+        ecmaVersion: 6,
+        sourceType: 'module'
+    }
+});
+ruleTester.run('path-checker', rule, {
+    valid: [
+        {
+            filename: '/home/johnk/Рабочий стол/repositories/production-TS-ReactJS-project/src/entities/Article',
+            code: 'import { addCommentFormActions, addCommentFormReducer } from "../../model/slice/addCommentFormSlice";',
+            errors: [],
+        },
+    ],
 
-const ruleTester = new RuleTester();
-ruleTester.run("path-checker", rule, {
-  valid: [
-    // give me some code that won't trigger a warning
-  ],
-
-  invalid: [
-    {
-      code: "asdf",
-      errors: [{ message: "Fill me in.", type: "Me too" }],
-    },
-  ],
+    invalid: [
+        {
+            filename: '/home/johnk/Рабочий стол/repositories/production-TS-ReactJS-project/src/entities/Article',
+            code: 'import { addCommentFormActions, addCommentFormReducer } from "entities/Article/model/slice/addCommentFormSlice";',
+            errors: [{ message: 'В рамках одного слайса импорты должны быть относительными' }],
+        },
+    ],
 });
