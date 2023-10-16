ebb21121855e1cb6ce591066874a9fce4eb4f752
johnk2280 <johnk2280@gmail.com>
2023-10-16 18:49:42 +0500

    [add] public-api-imports rule

---
+++ tests/lib/rules/public-api-imports.js
@@ -0,0 +1,31 @@
+/**
+ * @fileoverview rule of public api imports
+ * @author johnk
+ */
+"use strict";
+
+//------------------------------------------------------------------------------
+// Requirements
+//------------------------------------------------------------------------------
+
+const rule = require("../../../lib/rules/public-api-imports"),
+  RuleTester = require("eslint").RuleTester;
+
+
+//------------------------------------------------------------------------------
+// Tests
+//------------------------------------------------------------------------------
+
+const ruleTester = new RuleTester();
+ruleTester.run("public-api-imports", rule, {
+  valid: [
+    // give me some code that won't trigger a warning
+  ],
+
+  invalid: [
+    {
+      code: "",
+      errors: [{ message: "Fill me in.", type: "Me too" }],
+    },
+  ],
+});
